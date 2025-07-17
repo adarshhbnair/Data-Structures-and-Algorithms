aa356d422f2b900a3e5da56bdc152903167b5e47
adarshhbnair <adarshhbnair@gmail.com>
2025-07-17 23:59:10 +0530

    Linked List

---
+++ addTwoNumbers_LL.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode(int x):val(x),next(nullptr){}
+};
+
+ListNode *addTwoNumbers(ListNode *l1, ListNode *l2){
+    ListNode *dummy = new ListNode(0);
+    ListNode *curr = dummy;
+    int carry = 0;
+
+    while(l1 != nullptr || l2 != nullptr){
+        int sum = carry;
+
+        if(l1 != nullptr){
+            sum +=l1->val;
+            l1 = l1->next;
+        }
+        if(l1 != nullptr){
+            sum += l2->val;
+            l2 = l2->next;
+        }
+        carry = sum/10;
+        curr->next = new ListNode(sum % 10);
+        curr = curr->next;
+    }
+
+    if(carry>0){
+        curr->next = new ListNode(carry);
+    }
+    return dummy->next;
+}
+
+void printList(ListNode *head){
+    while(head != nullptr){
+        cout<<head->val;
+        if(head->next != nullptr){
+            cout<<" ->";
+        }
+        head = head->next;
+    }
+    cout<<endl;
+}
+
+int main(){
+    ListNode *l1 = new ListNode(2);
+    l1->next = new ListNode(4);
+    l1->next->next = new ListNode(3);
+
+    ListNode *l2 = new ListNode(5);
+    l2->next = new ListNode(6);
+    l2->next->next = new ListNode(4);
+
+    ListNode *result = addTwoNumbers(l1,l2);
+    printList(result);
+    return 0;
+}
